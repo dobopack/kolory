640fbd4678439bca2ae3125884ee9da3dcaa2ff7
Piotr Kalinowski <pikalin@gmail.com>
2021-09-25 23:43:06 +0200

    _app.js added Layout component and added passing isMain variable

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const isMain = Object.values(router.query).length > 0 ? false : true;
+
+  return (
+    <Layout isMain={isMain}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
