c0f0856ab0deca86d102cbfd2d7e014d04515a10
Piotrek <piotrek@DESKTOP-1IPG09S.localdomain>
2021-09-25 23:40:13 +0200

    added Hero section

---
+++ components/landing/HeroSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+import Section from "../ui/Section";
+import classes from "./HeroSection.module.css";
+
+import removeHash from "../../removeHash";
+
+function HeroSection() {
+  return (
+    <div className={classes.hero}>
+      <Section className={classes.heroContent}>
+        <h1>Sięgnij po intensywny kolor, zapach i smak</h1>
+        <p>
+          Aromaty, barwniki spożywcze, bazy do napojów i oleorezyny dla
+          przemysłu
+        </p>
+        <div className={classes.buttonSection} onClick={removeHash}>
+          <Link href="/#productSection">
+            <button className={classes.productsButton}>Sprawdź produkty</button>
+          </Link>
+          <Link href="/#formSection">
+            <button className={classes.ctaButton}>Napisz do nas</button>
+          </Link>
+        </div>
+      </Section>
+    </div>
+  );
+}
+
+export default HeroSection;
