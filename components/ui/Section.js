857a34c422f2526a9f80ce35c7529570fc6076c3
Piotr Kalinowski <pikalin@gmail.com>
2021-09-26 02:09:21 +0200

    added id param in Section component

--- components/ui/Section.js
+++ components/ui/Section.js
@@ -4,7 +4,7 @@ import classes from "./Section.module.css";
 
 function Section(props) {
   return (
-    <div className={`${classes.section} ${props.className}`}>
+    <div className={`${classes.section} ${props.className}`} id={props.id}>
       {props.children}
     </div>
   );
