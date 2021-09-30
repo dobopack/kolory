0fc7cef459581be9dbabf597f0622772245f15d6
Piotr Kalinowski <pikalin@gmail.com>
2021-09-30 19:22:11 +0200

    added id in ProductsSection

--- components/landing/ProductsSection.js
+++ components/landing/ProductsSection.js
@@ -8,7 +8,7 @@ import classes from "./ProductsSection.module.css";
 
 function ProductsSection({ categories }) {
   return (
-    <Section className={classes.productsSection}>
+    <Section className={classes.productsSection} id="productSection">
       <Header>Nasze produkty</Header>
       <p>
         Sprzedawane przez nas dodatki spożywcze są substancjami skoncentrowanymi
