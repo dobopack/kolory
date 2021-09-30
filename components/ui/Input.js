1ce58fceaac5b81dd39cc3bd758c9391cf9fc85b
Piotr Kalinowski <pikalin@gmail.com>
2021-09-30 19:13:14 +0200

    added Input component

---
+++ components/ui/Input.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+function Input(props) {
+  return (
+    <div className={classes.wrapper}>
+      <input
+        className={`${classes.formInput} ${props.className}`}
+        id={props.id}
+        name={props.name}
+        // autoComplete="off"
+        value={props.value}
+        onChange={props.onChangeText}
+        type={props.type}
+        placeholder={props.placeholder}
+        rows={props.rows}
+        required={props.required}>
+        {props.children}
+      </input>
+      <span className={classes.underline}></span>
+    </div>
+  );
+}
+
+export default Input;
