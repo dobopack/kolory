6aa0db65940c3fafc67e7d38f5929460afa4bc63
Piotr Kalinowski <pikalin@gmail.com>
2021-09-30 19:13:35 +0200

    added Textarea component

---
+++ components/ui/Textarea.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import classes from "./Textarea.module.css";
+
+function Input(props) {
+  return (
+    <div className={classes.wrapper}>
+      <textarea
+        className={`${classes.formInput} ${props.className}`}
+        id={props.id}
+        name={props.name}
+        value={props.value}
+        onChange={props.onChangeText}
+        placeholder={props.placeholder}
+        rows={props.rows}>
+        {props.children}
+      </textarea>
+      <span className={classes.underline}></span>
+    </div>
+  );
+}
+
+export default Input;
