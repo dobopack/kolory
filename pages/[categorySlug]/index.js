c826b3f517d4761a28bb83a792e86362a03ec683
Piotr Kalinowski <pikalin@gmail.com>
2021-10-07 22:12:06 +0200

    added Header and CategorySection in CategoryPage. Added is_active filter in products query

--- pages/[categorySlug]/index.js
+++ pages/[categorySlug]/index.js
@@ -1,21 +1,34 @@
+import Head from "next/head";
+
 import gql from "graphql-tag";
 import client from "../../apolloClient";
 
 import Section from "../../components/ui/Section";
 import Header from "../../components/ui/Header";
+import CategorySection from "../../components/categories/CategorySection";
+
+import classes from "../../styles/category.module.css";
 
 export default function CategoryPage({ category }) {
   return (
-    <Section>
-      <Header>{category.name}</Header>
-      <ul>
-        {category.product.map((prod, i) => (
-          <li key={i}>
-            <a href={`${category.slug}/${prod.slug}`}>{prod.name}</a>
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Head>
+        <title>
+          Dobopack - dystrybutor dodatków barwiących i smakowo-zapachowych do
+          żywności
+        </title>
+        <meta
+          name="description"
+          content="Dobopack Trading - oferujemy dodatki do żywności dla producentów: barwniki spożywcze naturalne i syntetyczne, aromaty, oleorezyny, olejki eteryczne, bazy do napojów. +48 22 633 96 27"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Section className={classes.categorySection}>
+        <Header>{category.name}</Header>
+        <p className={classes.description}>{category.description}</p>
+        <CategorySection category={category} />
+      </Section>
+    </>
   );
 }
 
@@ -52,7 +65,8 @@ export async function getStaticProps({ params }) {
           id
           name
           slug
-          product {
+          description
+          product(where: { is_active: true }) {
             id
             name
             slug
