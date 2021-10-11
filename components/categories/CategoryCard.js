32417ada7aaee66b1bccb28660a4cf88aa20dcfb
Piotr Kalinowski <pikalin@gmail.com>
2021-10-11 23:19:37 +0200

    added color mode in CategoryCard

--- components/categories/CategoryCard.js
+++ components/categories/CategoryCard.js
@@ -3,15 +3,33 @@ import Image from "next/image";
 import Link from "next/link";
 
 import imageTemplate from "../../public/image-template.svg";
+import imageBottle from "../../public/bottle.jpg";
 
 import classes from "./CategoryCard.module.css";
 
 function CategoryCard({ category, product }) {
-  const imageUrl = product.image ? product.image.url : imageTemplate;
+  let imageUrl = "";
+  if (product.color) {
+    imageUrl = imageBottle;
+  } else if (product.image) {
+    imageUrl = product.image.url;
+  } else {
+    imageUrl = imageTemplate;
+  }
+
+  const colorCircle = product.color ? (
+    <span
+      className={classes.colorCircle}
+      style={{ backgroundColor: `#${product.color}` }}></span>
+  ) : (
+    ""
+  );
+
   return (
     <div className={classes.cardWrapper}>
       <Link href={`/${category.slug}/${product.slug}`} passHref={true}>
         <div className={classes.cardInner}>
+          {colorCircle}
           <div className={classes.cardImage}>
             <Image src={imageUrl} alt={product.name} layout="fill" />
           </div>
