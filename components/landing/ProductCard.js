c8414f6e187b6bc54fd6d1befa45af7cd2b6f142
Piotrek <pikalin@protonmail.com>
2021-11-08 19:52:41 +0100

    ProductCard added image check changed line-height in h2

--- components/landing/ProductCard.js
+++ components/landing/ProductCard.js
@@ -4,13 +4,22 @@ import Link from "next/link";
 
 import classes from "./ProductCard.module.css";
 
+import imageTemplate from "../../public/image-template.svg";
+
 function ProductCard({ category }) {
+  let imageUrl = "";
+  if (category.image) {
+    imageUrl = category.image.url;
+  } else {
+    imageUrl = imageTemplate;
+  }
+
   return (
     <Link href={`/${category.slug}`} passHref={true}>
       <div className={classes.cardContainer}>
         <div className={classes.cardInner}>
           <div className={classes.imageContainer}>
-            <Image src={category.image.url} alt={category.name} layout="fill" />
+            <Image src={imageUrl} alt={category.name} layout="fill" />
           </div>
           <div className={classes.headerContainer}>
             <h2>{category.name}</h2>
