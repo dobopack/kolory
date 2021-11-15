e436425bb835cd1beb77625b5b06ca8842830360
Piotr Kalinowski <pikalin@gmail.com>
2021-11-15 19:58:15 +0100

    changed ProductsSection order, changed media-queries

--- components/landing/ProductsSection.js
+++ components/landing/ProductsSection.js
@@ -10,12 +10,12 @@ function ProductsSection({ categories, config }) {
   return (
     <Section className={classes.productsSection} id="productSection">
       <Header>Nasza oferta</Header>
-      <p>{config.products_description}</p>
       <div className={classes.cardsBox}>
         {categories.map((category, i) => (
           <ProductCard key={i} category={category} />
         ))}
       </div>
+      <p>{config.products_description}</p>
     </Section>
   );
 }
