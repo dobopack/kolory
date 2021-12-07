fa1c2788f0028fcc202299005b3d316ae9c4fde0
Piotrek <pikalin@protonmail.com>
2021-12-07 22:32:39 +0100

    index.js added keywords and lang tags to header

--- pages/[categorySlug]/[productSlug]/index.js
+++ pages/[categorySlug]/[productSlug]/index.js
@@ -9,6 +9,7 @@ export default function ProductPage({ product }) {
   return (
     <>
       <Head>
+        <html lang="pl-PL" />
         <title>
           Dobopack - dystrybutor dodatków barwiących i smakowo-zapachowych do
           żywności
@@ -16,6 +17,10 @@ export default function ProductPage({ product }) {
         <meta
           name="description"
           content="Dobopack Trading - oferujemy dodatki do żywności dla producentów: barwniki spożywcze naturalne i syntetyczne, aromaty, oleorezyny, olejki eteryczne, bazy do napojów. +48 22 633 96 27"
+        />
+        <meta
+          name="keywords"
+          content="Barwnik spożywczy, Barwniki spożywcze, Barwniki spożywcze w proszku, Aromaty spożywcze, Oleorezyny, Olejki eteryczne, tartrazyna E102, żółcień chinolinowa E104, żółcień pomarańczowa E110, azorubina E122, czerwień koszenilowa E124, erytrozyna E127, czerwień Allura E129, błękit patentowy E131, indygotyna E132, błękit brylantowy E133, zieleń S E142, czerń brylantowa E151, brąz HT E155, barwnik kurkumina, barwnik chlorofilina, bazy do napojów, ekstrakty przyprawowe, oleorezyna capsicum, olejek czarnego pieprzu, olejek lawendowy, olejek miętowy, oleorezyna czarnego pieprzu, oleorezyna białego pieprzu, oleorezyna papryki, oleorezyna kminku zwyczajnego, oleorezyna goździka, oleorezyna gałki muszkatołowej, bazy i aromaty do napojów BCAA, bazy i aromaty do napojów energetyzujących, bazy i aromaty do napojów z wodą kokosową, bazy i aromaty do napojów słodowych, bazy i aromaty do napojów float, aromaty i bazy do napojów cydrowych, zamiennik bieli tytanowej, aromaty, proszkowe, aromaty płynne, aromaty granulowane"
         />
         <link rel="icon" href="/favicon.svg" />
       </Head>
--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ export default function Home({ categories, config }) {
   return (
     <div>
       <Head>
+        <html lang="pl-PL" />
         <title>
           Dobopack - dystrybutor dodatków barwiących i smakowo-zapachowych do
           żywności
@@ -30,6 +31,10 @@ export default function Home({ categories, config }) {
         <meta
           name="description"
           content="Dobopack Trading - oferujemy dodatki do żywności dla producentów: barwniki spożywcze naturalne i syntetyczne, aromaty, oleorezyny, olejki eteryczne, bazy do napojów. +48 22 633 96 27"
+        />
+        <meta
+          name="keywords"
+          content="Barwnik spożywczy, Barwniki spożywcze, Barwniki spożywcze w proszku, Aromaty spożywcze, Oleorezyny, Olejki eteryczne, tartrazyna E102, żółcień chinolinowa E104, żółcień pomarańczowa E110, azorubina E122, czerwień koszenilowa E124, erytrozyna E127, czerwień Allura E129, błękit patentowy E131, indygotyna E132, błękit brylantowy E133, zieleń S E142, czerń brylantowa E151, brąz HT E155, barwnik kurkumina, barwnik chlorofilina, bazy do napojów, ekstrakty przyprawowe, oleorezyna capsicum, olejek czarnego pieprzu, olejek lawendowy, olejek miętowy, oleorezyna czarnego pieprzu, oleorezyna białego pieprzu, oleorezyna papryki, oleorezyna kminku zwyczajnego, oleorezyna goździka, oleorezyna gałki muszkatołowej, bazy i aromaty do napojów BCAA, bazy i aromaty do napojów energetyzujących, bazy i aromaty do napojów z wodą kokosową, bazy i aromaty do napojów słodowych, bazy i aromaty do napojów float, aromaty i bazy do napojów cydrowych, zamiennik bieli tytanowej, aromaty, proszkowe, aromaty płynne, aromaty granulowane"
         />
         <link rel="icon" href="/favicon.svg" />
       </Head>
