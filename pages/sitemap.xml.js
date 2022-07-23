1adb0a0b6d9b7c3bb5368a8fabcbe26399edf68c
Piotrek <pikalin@protonmail.com>
2022-07-23 23:58:37 +0200

    sitemap.xml added images

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -9,19 +9,25 @@ export const getServerSideProps = async ({ res }) => {
     production: "https://www.kolory.com",
   }[process.env.NODE_ENV];
 
-  const staticPages = ["", "ochrona-danych", "polityka"].map(
-    (staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    }
-  );
+  const staticImages = [
+    `${baseUrl}/hero-image.jpg`,
+    `${baseUrl}/logo-white.svg`,
+    `${baseUrl}/logo-dark.svg`,
+  ];
 
   const { data } = await client.query({
     query: gql`
       query {
         categories(where: { is_active: true }) {
           slug
+          image {
+            url
+          }
           product(where: { is_active: true }) {
             slug
+            image {
+              url
+            }
           }
         }
       }
@@ -30,21 +36,71 @@ export const getServerSideProps = async ({ res }) => {
 
   const { categories } = data;
   const dynamicPages = [];
+  // category images on main page
+  const categoryImages = [];
+  // product images on category page
+  let productImages = {};
 
   categories.forEach((cat) => {
-    dynamicPages.push(`${baseUrl}/${cat.slug}`);
+    categoryImages.push(cat.image.url);
+    dynamicPages.push({
+      type: "category",
+      url: `${baseUrl}/${cat.slug}`,
+      image: cat.image.url,
+    });
     cat.product.forEach((prod) => {
-      dynamicPages.push(`${baseUrl}/${cat.slug}/${prod.slug}`);
+      let url = `${baseUrl}/${cat.slug}`;
+
+      if (!productImages[url]) productImages[url] = [];
+      productImages[url].push(prod.image.url);
+
+      dynamicPages.push({
+        type: "product",
+        url: `${url}/${prod.slug}`,
+        image: prod.image.url,
+      });
     });
   });
 
+  const staticPages = [
+    { url: "", image: [...categoryImages, ...staticImages] },
+  ].map(({ url, image }) => {
+    return { url: `${baseUrl}/${url}`, image: image };
+  });
+
+  const getImages = (type, url, image) => {
+    let result = "";
+    if (type === "product") {
+      result = `<image:image>
+              <image:loc>${image}</image:loc>
+            </image:image>`;
+    } else if (type === "category") {
+      if (productImages[url]) {
+        productImages[url].map((imageUrl) => {
+          result += `<image:image>
+              <image:loc>${imageUrl}</image:loc>
+            </image:image>`;
+        });
+      }
+    } else if (type === "static") {
+      image.map((imageUrl) => {
+        result += `<image:image>
+              <image:loc>${imageUrl}</image:loc>
+            </image:image>`;
+      });
+    }
+
+    return result;
+  };
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
       ${staticPages
-        .map((url) => {
+        .map(({ url, image }) => {
           return `
             <url>
               <loc>${url}</loc>
+              ${getImages("static", url, image)}
               <lastmod>${new Date().toISOString()}</lastmod>
               <changefreq>daily</changefreq>
               <priority>1.0</priority>
@@ -53,10 +109,11 @@ export const getServerSideProps = async ({ res }) => {
         })
         .join("")}
         ${dynamicPages
-          .map((url) => {
+          .map(({ type, url, image }) => {
             return `
           <url>
             <loc>${url}</loc>
+            ${getImages(type, url, image)}
             <lastmod>${new Date().toISOString()}</lastmod>
             <changefreq>daily</changefreq>
             <priority>1.0</priority>
