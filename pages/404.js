810215c0f10740e4f4a22a68c9bb5fdab4f6a7c0
Piotrek <pikalin@protonmail.com>
2022-07-23 22:22:11 +0200

    changed 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import Link from "next/link";
+
+import classes from "../styles/404.module.css";
 
 export default function Custom404() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/");
-  });
-
-  return null;
+  return (
+    <div className={classes.container}>
+      <h1>404</h1>
+      <h2 className={classes.titleHeader}>Strona nie znaleziona</h2>
+      <Link href="/">
+        <a>Wróć na stronę główną</a>
+      </Link>
+    </div>
+  );
 }
