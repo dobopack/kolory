07783db0a1d3edaccc1b16172c1cf727782381eb
Piotrek <pikalin@protonmail.com>
2022-07-23 22:54:13 +0200

    added redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/\\?t=aboutSection",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/\\?t=productSection",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/\\?t=footerSection",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/\\?t=formSection",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/ochrona_danych",
+        destination: "/ochrona-danych",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com", "media.graphassets.com"],
