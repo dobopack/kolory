b70e782f76f975c5325b16399c12bb77787a8671
Piotrek <pikalin@protonmail.com>
2022-07-24 23:33:25 +0200

    AboutSection changed images path

--- components/landing/AboutSection.js
+++ components/landing/AboutSection.js
@@ -1,23 +1,19 @@
 import React from "react";
-import Image from "next/image";
 
-import aboutPartners from "../../public/about-partners.svg";
-import aboutControl from "../../public/about-control.svg";
-import aboutRequirements from "../../public/about-requirements.svg";
-import imageTemplate from "../../public/image-template.svg";
+// import imageTemplate from "../../public/image-template.svg";
 
 import Section from "../ui/Section";
 import Header from "../ui/Header";
 import AboutCard from "./AboutCard";
 import classes from "./AboutSection.module.css";
 
-const getImageUrl = (image) => {
-  if (image) {
-    return image.url;
-  } else {
-    return imageTemplate;
-  }
-};
+// const getImageUrl = (image) => {
+//   if (image) {
+//     return image.url;
+//   } else {
+//     return imageTemplate;
+//   }
+// };
 
 function AboutSection({ config }) {
   return (
@@ -26,7 +22,7 @@ function AboutSection({ config }) {
       <div className={classes.cardSection}>
         <AboutCard
           className={classes.firstCard}
-          image={getImageUrl(config.card1_image)}
+          image="/about-card-1.svg"
           imgHeight={110}
           imgWidth={110}
           alt={config.card1_header}
@@ -34,7 +30,7 @@ function AboutSection({ config }) {
           paragraph={config.card1_content}
         />
         <AboutCard
-          image={getImageUrl(config.card2_image)}
+          image="/about-card-2.svg"
           imgHeight={110}
           imgWidth={110}
           alt={config.card2_header}
@@ -43,7 +39,7 @@ function AboutSection({ config }) {
         />
         <AboutCard
           className={classes.lastCard}
-          image={getImageUrl(config.card3_image)}
+          image="/about-card-3.svg"
           imgHeight={110}
           imgWidth={110}
           alt={config.card3_header}
