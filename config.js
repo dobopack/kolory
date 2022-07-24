63f931674b90ad611f7657f31259ef7d1912ef09
Piotrek <pikalin@protonmail.com>
2022-07-24 22:48:49 +0200

    config.js added baseUrl

--- config.js
+++ config.js
@@ -5,4 +5,11 @@ const config = {
     "Dobopack Trading - oferujemy dodatki do żywności dla producentów: barwniki spożywcze naturalne i syntetyczne, aromaty, oleorezyny, olejki eteryczne, bazy do napojów. +48 22 633 96 27",
 };
 
+const baseUrl = {
+  development: "http://localhost:3000",
+  production: "https://www.kolory.com",
+}[process.env.NODE_ENV];
+
+config.baseUrl = baseUrl;
+
 export default config;
