3853865e719204a49911730f237a9a16214abad8
Piotrek <pikalin@protonmail.com>
2022-08-01 15:36:56 +0200

    added second google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,22 @@ export default class MyDocument extends Document {
             }}
           />
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_2}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_2}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
               __html: JSON.stringify({
