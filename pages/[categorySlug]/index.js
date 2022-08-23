ca1834127b4699363890e26b5bdbb5f89a47b510
Piotr Kalinowski <pikalin@gmail.com>
2022-08-23 20:03:11 +0200

    changed to getServerSideProps for canonical and pagination

--- pages/[categorySlug]/index.js
+++ pages/[categorySlug]/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import ErrorPage from "next/error";
+import Custom404 from "../404";
 
 import gql from "graphql-tag";
 import client from "../../apolloClient";
 import configData from "../../config";
-
-import { useState, useEffect } from "react";
 
 import Section from "../../components/ui/Section";
 import Header from "../../components/ui/Header";
@@ -15,53 +13,15 @@ import Breadcrumbs from "../../components/ui/Breadcrumbs";
 
 import classes from "../../styles/category.module.css";
 
-export default function CategoryPage({ category, slug, config }) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [maxPage, setMaxPage] = useState(1);
-  const [err, setErr] = useState(false);
-
-  const rootPage = `${configData.baseUrl}/${slug}`;
-
-  useEffect(() => {
-    const maxPage = Math.ceil(category.product.length / 8);
-
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-
-    let queryPage;
-    if (params.p) {
-      if (+params.p < 1 || +params.p > maxPage) {
-        setErr(true);
-      } else {
-        queryPage = +params.p;
-      }
-    } else {
-      queryPage = 1;
-    }
-
-    setCurrentPage(queryPage);
-    setMaxPage(maxPage);
-  }, [category.product.length]);
-
-  let prev, next, canonical;
-
-  if (currentPage < maxPage) {
-    next = `${rootPage}?p=${currentPage + 1}`;
-  } else {
-    next = null;
-  }
-
-  if (currentPage == 1) {
-    canonical = rootPage;
-    prev = null;
-  }
-  if (currentPage > 1) {
-    canonical = `${rootPage}?p=${currentPage}`;
-    if (currentPage <= 2) {
-      prev = rootPage;
-    } else {
-      prev = `${rootPage}?p=${currentPage - 1}`;
-    }
+export default function CategoryPage({
+  category,
+  slug,
+  config,
+  canonical,
+  queryPage,
+}) {
+  if (!category || !queryPage) {
+    return <Custom404 />;
   }
 
   let title;
@@ -101,68 +61,43 @@ export default function CategoryPage({ category, slug, config }) {
 
   return (
     <>
-      {err && <ErrorPage statusCode={404} />}
-      {!err && (
-        <>
-          <Head>
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={category.image.url} />
-            <meta property="og:url" content={currentUrl} />
-            <link rel="icon" href="/favicon.svg" />
-            {prev && <link rel="prev" href={prev} />}
-            {next && <link rel="next" href={next} />}
-            {canonical && <link rel="canonical" href={canonical} />}
-            <script
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{
-                __html: JSON.stringify({
-                  "@context": "https://schema.org",
-                  "@type": "BreadcrumbList",
-                  itemListElement: breadcrumbsLinks,
-                }),
-              }}
-            />
-          </Head>
-          <Breadcrumbs links={breadcrumbsLinks} />
-          <Section className={classes.categorySection}>
-            <Header>{category.name}</Header>
-            <h2 className={classes.description}>{category.description}</h2>
-            <CategorySection category={category} slug={slug} />
-          </Section>
-        </>
-      )}
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={category.image.url} />
+        <meta property="og:url" content={currentUrl} />
+        <link rel="icon" href="/favicon.svg" />
+        {canonical.prev && <link rel="prev" href={canonical.prev} />}
+        {canonical.next && <link rel="next" href={canonical.next} />}
+        {canonical.canonical && (
+          <link rel="canonical" href={canonical.canonical} />
+        )}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: breadcrumbsLinks,
+            }),
+          }}
+        />
+      </Head>
+      <Breadcrumbs links={breadcrumbsLinks} />
+      <Section className={classes.categorySection}>
+        <Header>{category.name}</Header>
+        <h2 className={classes.description}>{category.description}</h2>
+        <CategorySection category={category} slug={slug} />
+      </Section>
     </>
   );
 }
 
-export async function getStaticPaths() {
-  const { data } = await client.query({
-    query: gql`
-      query {
-        categories {
-          slug
-        }
-      }
-    `,
-  });
-
-  const { categories } = data;
-
-  const paths = categories.map((category) => ({
-    params: { categorySlug: category.slug },
-  }));
-
-  return {
-    paths,
-    fallback: "blocking",
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const slug = params.categorySlug;
+export async function getServerSideProps({ query }) {
+  const slug = query.categorySlug;
+  const page = query.p || 1;
 
   const newsQuery = slug === "nowosci" ? "last: 8, " : "";
 
@@ -195,8 +130,7 @@ export async function getStaticProps({ params }) {
 
   const { categories } = data;
 
-  const category = categories[0];
-  const notFound = category ? false : true;
+  const category = categories[0] || null;
 
   const confData = await client.query({
     query: gql`
@@ -211,13 +145,51 @@ export async function getStaticProps({ params }) {
 
   const { config } = confData.data;
 
+  let queryPage = +page;
+  let canonical = {
+    prev: null,
+    next: null,
+    canonical: null,
+  };
+
+  if (category) {
+    const rootPage = `${configData.baseUrl}/${slug}`;
+
+    const maxPage = Math.ceil(category.product.length / 8);
+
+    if (+page < 1 || +page > maxPage) {
+      queryPage = null;
+    }
+
+    if (queryPage) {
+      if (queryPage < maxPage) {
+        canonical.next = `${rootPage}?p=${queryPage + 1}`;
+      } else {
+        canonical.next = null;
+      }
+
+      if (queryPage == 1) {
+        canonical.canonical = rootPage;
+        canonical.prev = null;
+      }
+      if (queryPage > 1) {
+        canonical.canonical = `${rootPage}?p=${queryPage}`;
+        if (queryPage <= 2) {
+          canonical.prev = rootPage;
+        } else {
+          canonical.prev = `${rootPage}?p=${queryPage - 1}`;
+        }
+      }
+    }
+  }
+
   return {
     props: {
       category,
       slug,
       config,
+      canonical,
+      queryPage,
     },
-    notFound,
-    revalidate: 1,
   };
 }
